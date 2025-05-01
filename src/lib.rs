67c16c4cff29f538623ebaaf95441083ed12e343
sldpzshdwz <At19001214@outlook.com>
2025-05-01 11:15:19 +0800

    修改线程池数量

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod network;
 pub mod login;
 pub mod tool;
 pub mod url;
-static 线程池线程数:u32=5;
+static 线程池线程数:u32=50;
 static 数据库连接url: &str = "mysql://root:aA147741@localhost:3306/rust91";
 static 日志显示级别:日志级别=日志级别::INFO;
 static deepseek_api:&str="sk-b0b8e26ba39040f690e56e2ad892b7f5";
